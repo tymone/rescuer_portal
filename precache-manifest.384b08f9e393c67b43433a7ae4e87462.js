f5326995d2919f75b40cba7c0a860ed71c5770c7
tymone <tymoteusz.hryniewiecki@gmail.com>
2019-07-24 17:45:52 +0200

    Updates

---
+++ precache-manifest.384b08f9e393c67b43433a7ae4e87462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa860d786fcd99ffcc5a",
+    "url": "/rescuer_portal/static/css/main.230a031e.chunk.css"
+  },
+  {
+    "revision": "fa860d786fcd99ffcc5a",
+    "url": "/rescuer_portal/static/js/main.d032a52b.chunk.js"
+  },
+  {
+    "revision": "af33ccc55540f1dc8c0d",
+    "url": "/rescuer_portal/static/js/runtime~main.819c09d5.js"
+  },
+  {
+    "revision": "81c8ccd6c666ed072b72",
+    "url": "/rescuer_portal/static/js/2.c29ce0b4.chunk.js"
+  },
+  {
+    "revision": "2acceef1b97f3078ed1910845d5fc25c",
+    "url": "/rescuer_portal/index.html"
+  }
+];
